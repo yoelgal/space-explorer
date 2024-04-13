edda618e851f4440d0cb643c548d51d6fef40b52
yoelgal <yoelgal108@gmail.com>
2024-04-13 10:49:41 +0100

    feat: added .prettierrc and some assets

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -11,11 +11,7 @@ document.body.appendChild(renderer.domElement);
 const loader = new GLTFLoader();
 const planets = [];
 const planetInfo = [
-    { name: 'Earth', distance: 40, speed: 0.02, scale: [0.01,0.01,0.01] },
-    { name: 'Jupiter', distance: 80, speed: 0.018, scale: [0.05,0.05,0.05] },
-    { name: 'Neptune', distance: 120, speed: 0.015, scale: [1,1,1] },
-    { name: 'Planet4', distance: 400, speed: 0.012, scale: [0.1,0.1,0.1] },
-    { name: 'Planet5', distance: 500, speed: 0.01, scale: [0.1,0.1,0.1] },
+    { name: 'Earth', distance: 100, speed: 0.02, scale: [1,1,1] },
 ];
 
 // Ambient Light
@@ -25,27 +21,30 @@ scene.add(ambientLight);
 // Load the Sun
 loader.load('Sun.glb', (gltf) => {
     const sun = gltf.scene;
-    sun.scale.set(10, 10, 10); // Adjust scale for visibility
+    sun.scale.set(50, 50, 50); // Adjust scale for visibility
     sun.position.set(0, 0, 0);
     scene.add(sun);
 });
 
 // Load planets
-planetInfo.forEach(info => {
-    loader.load(`${info.name}.glb`, (gltf) => {
+planetInfo.forEach(planet => {
+    loader.load(`${planet.name}.glb`, (gltf) => {
         const planet = gltf.scene;
-        planet.scale.set(...info.scale); // Scale down for appropriate size
-        planet.position.x = info.distance;
+        planet.scale.set(...planet.scale); // Scale down for appropriate size
+        planet.position.x = planet.distance;
         scene.add(planet);
-        planets.push({ mesh: planet, speed: info.speed/2, distance: info.distance, angle: 0 });
+        planets.push({ mesh: planet, speed: planet.speed/2, distance: planet.distance, angle: 0 });
     });
 });
 
-// OrbitControls to pivot around the Sun
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 0, 0);
-camera.position.set(0, 150, 300);
-controls.update();
+
+camera.position.set(0.08343285914438951, 335.3059565045275, 8.361134584523754);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+/*
+x:0.08343285914438951
+y:335.3059565045275
+z:8.361134584523754
+*/
 
 // Update planets in their orbits
 function animate() {
